b7c6c6faea72170fbcc7dceeddf44f09fd2706a3
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-09-02 16:39:57 +0100

    [CI] split trigger pipeline events

---
+++ .ci/trigger-builds.groovy
@@ -0,0 +1,82 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+@Library('apm@current') _
+
+pipeline {
+  agent { label 'master' }
+  environment {
+    NOTIFY_TO = credentials('notify-to')
+    PIPELINE_LOG_LEVEL = 'INFO'
+    BEATS_TESTER_JOB = 'Beats/beats-tester-mbp/master'
+    VERSION = '8.0.0-SNAPSHOT'
+  }
+  options {
+    timeout(time: 1, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20'))
+    timestamps()
+    ansiColor('xterm')
+    disableResume()
+    durabilityHint('PERFORMANCE_OPTIMIZED')
+  }
+  triggers {
+    cron('H H(4-5) * * 1-5')
+    upstream('Beats/beats/master')
+  }
+  stages {
+    stage('Run Tasks on a cron basis'){
+      when {
+        triggeredBy 'TimerTrigger'
+      }
+      steps {
+        build(job: env.BEATS_TESTER_JOB, propagate: false, wait: false)
+      }
+    }
+    stage('Run Tasks on an Upstream basis'){
+      when {
+        triggeredBy 'UpstreamCause'
+      }
+      steps {
+        git 'https://github.com/elastic/beats-tester.git'
+        sh(label: 'Get snapshot metadata', script: ".ci/scripts/fetch-metadata.sh ${VERSION}")
+        // Archive metadata to be compared in the upstream setup stage.
+        archiveArtifacts(allowEmptyArchive: false, artifacts: 'metadata.txt')
+        copyArtifacts(projectName: env.BEATS_TESTER_JOB, filter: 'metadata.txt', target: 'previous', optional: true)
+        whenTrue(fileExists('previous/metadata.txt')) {
+          script {
+            env.NEW_CHANGES = sh(label: 'Compare metadata', returnStdout: true,
+                                script: 'cmp metadata.txt previous/metadata.txt && echo "false" || echo "true"')?.trim()
+            whenTrue(env.NEW_CHANGES.equals('true')) {
+              def buildId = sh(script: '.ci/scripts/get-build-id.sh metadata.txt', returnStdout: true)?.trim()
+              build(job: env.BEATS_TESTER_JOB, propagate: false, wait: false,
+                    parameters: [
+                      string(name: 'APM_URL_BASE', value: "https://staging.elastic.co/${buildId}/downloads/apm-server"),
+                      string(name: 'BEATS_URL_BASE', value: "https://staging.elastic.co/${buildId}/downloads/beats"),
+                      string(name: 'VERSION', value: env.VERSION)
+                    ])
+            }
+          }
+        }        
+      }
+    }
+  }
+  post {
+    cleanup {
+      notifyBuildResult(prComment: false)
+    }
+  }
+}
